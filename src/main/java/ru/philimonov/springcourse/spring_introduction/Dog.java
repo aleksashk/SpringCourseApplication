99d9009929dda84efaa6313077337fdc88b8b3d2
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-12 17:12:56 +0300

    simple app without spring

---
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Dog.java
@@ -0,0 +1,7 @@
+package ru.philimonov.springcourse.spring_introduction;
+
+public class Dog {
+    public void say(){
+        System.out.println("Wow-wow");
+    }
+}
---
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Starter.java
@@ -0,0 +1,9 @@
+package ru.philimonov.springcourse.spring_introduction;
+
+public class Starter {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.say();
+    }
+}
