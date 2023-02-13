5f8d4a7dc1c6bcc6fa33af6eb93df4844869eebd
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-13 16:24:21 +0300

    Dependency Injection with setter

--- src/main/java/ru/philimonov/springcourse/spring_introduction/Person.java
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Person.java
@@ -3,13 +3,17 @@ package ru.philimonov.springcourse.spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
-        System.out.println("Create bean Person");
-        this.pet = pet;
+    public Person() {
+        System.out.println("Create empty bean Person");
     }
 
     public void callYourPet(){
         System.out.println("Hey my pet!!!");
         pet.say();
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person added Pet!!!");
+        this.pet = pet;
     }
 }
