bdeadab66731da84ed1f60d1dc2e614b6eb85381
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-13 17:11:28 +0300

    Bean scope: prototype is not for stateless objects

--- src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
@@ -6,13 +6,13 @@ public class Starter4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-//        Dog dog1 = context.getBean("myPet", Dog.class);
-//        Dog dog2 = context.getBean("myPet", Dog.class);
-//        System.out.println(dog1.getName());
-//        System.out.println(dog2.getName());
-//        dog1.setName("Name for pet1");
-//        System.out.println(dog1.getName());
-//        System.out.println(dog2.getName());
+        Dog dog1 = context.getBean("myPet", Dog.class);
+        Dog dog2 = context.getBean("myPet", Dog.class);
+        System.out.println(dog1.getName());
+        System.out.println(dog2.getName());
+        dog1.setName("Name for pet1");
+        System.out.println(dog1.getName());
+        System.out.println(dog2.getName());
 
     }
 }
