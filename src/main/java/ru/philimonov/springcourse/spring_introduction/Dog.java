b12bbcc340600f421062902fcc49f6b3beca5a40
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-13 17:23:40 +0300

    init and destroy method

--- src/main/java/ru/philimonov/springcourse/spring_introduction/Dog.java
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Dog.java
@@ -1,24 +1,21 @@
 package ru.philimonov.springcourse.spring_introduction;
 
-public class Dog implements Pet{
-
-    private String name;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+public class Dog implements Pet {
 
     public Dog() {
-        this.name = "unknown";
         System.out.println("Create bean Dog");
     }
 
     @Override
-    public void say(){
+    public void say() {
         System.out.println("Wow-wow");
+    }
+
+    public void init() {
+        System.out.println("Class Dog: init method");
+    }
+
+    public void dest() {
+        System.out.println("Class Dog: destroy method");
     }
 }
--- src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
@@ -5,15 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Starter4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
-
-        Dog dog1 = context.getBean("myPet", Dog.class);
-        Dog dog2 = context.getBean("myPet", Dog.class);
-        System.out.println(dog1.getName());
-        System.out.println(dog2.getName());
-        dog1.setName("Name for pet1");
-        dog2.setName("PET2");
-        System.out.println(dog1.getName());
-        System.out.println(dog2.getName());
+        try (context) {
+            Dog dog1 = context.getBean("myPet", Dog.class);
+            dog1.say();
+        }
 
     }
 }
