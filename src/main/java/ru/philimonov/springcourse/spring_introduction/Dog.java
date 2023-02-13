8d267beb2fa99958600af61ce20c5f8b0cb2f364
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-13 17:09:00 +0300

    Bean scope: singleton (default). Object is the same, singleton for stateless objects

--- src/main/java/ru/philimonov/springcourse/spring_introduction/Dog.java
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Dog.java
@@ -2,7 +2,18 @@ package ru.philimonov.springcourse.spring_introduction;
 
 public class Dog implements Pet{
 
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public Dog() {
+        this.name = "unknown";
         System.out.println("Create bean Dog");
     }
 
--- src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
@@ -6,11 +6,13 @@ public class Starter4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-        Pet pet1 = context.getBean("myPet", Pet.class);
-        Pet pet2 = context.getBean("myPet", Pet.class);
-        System.out.println("pet1 = pet2 -> " + (pet1 == pet2));
-        System.out.println(pet1);
-        System.out.println(pet2);
+        Dog dog1 = context.getBean("myPet", Dog.class);
+        Dog dog2 = context.getBean("myPet", Dog.class);
+        System.out.println(dog1.getName());
+        System.out.println(dog2.getName());
+        dog1.setName("Name for pet1");
+        System.out.println(dog1.getName());
+        System.out.println(dog2.getName());
 
     }
 }
