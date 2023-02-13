76cf10a28da151a615feb60da90f46bb0b0796d1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-13 17:02:30 +0300

    Bean scope: singleton (default). Object is the same

--- src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
@@ -6,5 +6,11 @@ public class Starter4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
 
+        Pet pet1 = context.getBean("myPet", Pet.class);
+        Pet pet2 = context.getBean("myPet", Pet.class);
+        System.out.println("pet1 = pet2 -> " + (pet1 == pet2));
+        System.out.println(pet1);
+        System.out.println(pet2);
+
     }
 }
