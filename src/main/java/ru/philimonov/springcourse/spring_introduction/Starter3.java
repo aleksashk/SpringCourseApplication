2b67e71b2a64f455c244bf0309d5e60a4996ff15
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-13 14:23:39 +0300

    create person with simple code and pet with context

--- src/main/java/ru/philimonov/springcourse/spring_introduction/Starter3.java
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Starter3.java
@@ -5,8 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Starter3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.property");
-        Pet pet = new Dog();
-        Person person = new Person(pet);
+//        Pet pet = new Dog();
+//        Person person = new Person(pet);
+        Pet myPet = context.getBean("myPet", Pet.class);
+        Person person = new Person(myPet);
         person.callYourPet();
     }
 }
