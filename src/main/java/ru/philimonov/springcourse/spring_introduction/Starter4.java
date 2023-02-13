3f7c046b84ec1439c0ef72d82fe58fcf4c7c51af
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-13 17:00:43 +0300

    Bean scope: singleton (default)

---
+++ src/main/java/ru/philimonov/springcourse/spring_introduction/Starter4.java
@@ -0,0 +1,10 @@
+package ru.philimonov.springcourse.spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Starter4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+    }
+}
