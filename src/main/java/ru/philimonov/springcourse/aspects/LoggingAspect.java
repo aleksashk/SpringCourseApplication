466b1dc62d090d5ab79722a652600488d7310089
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-17 15:12:32 +0300

    @Before("execution(public void returnBook())")

--- src/main/java/ru/philimonov/springcourse/aspects/LoggingAspect.java
+++ src/main/java/ru/philimonov/springcourse/aspects/LoggingAspect.java
@@ -8,8 +8,13 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public void get*())")
+    @Before("execution(public void getBook())")
     public void beforeGetBookAdvice() {
         System.out.println("beforeGetBookAdvice: попытка получить книгу!!!");
+    }
+
+    @Before("execution(public void returnBook())")
+    public void beforeReturnBookAdvice() {
+        System.out.println("beforeReturnBookAdvice: попытка return книгу!!!");
     }
 }
