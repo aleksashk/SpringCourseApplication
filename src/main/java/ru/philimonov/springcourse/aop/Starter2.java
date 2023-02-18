37b96dc4fa1139dbc72db978e41ad3e7755b95cc
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-18 13:19:06 +0300

    @AfterReturning

---
+++ src/main/java/ru/philimonov/springcourse/aop/Starter2.java
@@ -0,0 +1,19 @@
+package ru.philimonov.springcourse.aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class Starter2 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        try (context) {
+            University university = context.getBean("university", University.class);
+            university.addStudents();
+            List<Student> students = university.getStudents();
+            System.out.println(students);
+        }
+
+    }
+}
---
+++ src/main/java/ru/philimonov/springcourse/aop/University.java
@@ -0,0 +1,27 @@
+package ru.philimonov.springcourse.aop;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class University {
+    private List<Student> students = new ArrayList<>();
+
+    public void addStudents() {
+        Student st1 = new Student("Fl Aleksandr", 4, 9.7);
+        Student st2 = new Student("Jonson Aleksandr", 2, 5.3);
+        Student st3 = new Student("Grinch Aleksandr", 1, 6.8);
+
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+    }
+
+    public List<Student> getStudents() {
+        System.out.println("Information from method getStudents: ");
+        System.out.println(students);
+        return students;
+    }
+}
